34fd8eaec89973634bea002ace87bd473e7e6749
victoralvarado <alvaradoguzman900@gmail.com>
2022-10-15 12:16:19 -0600

    Update ScreenIniciarSesion.js

--- screens/ScreenIniciarSesion.js
+++ screens/ScreenIniciarSesion.js
@@ -11,19 +11,11 @@ export default function ScreenInicio(props) {
         colors: {
             // Add new color
             primary: {
-                50: '#E3F2F9',
-                100: '#C5E4F3',
-                200: '#A2D4EC',
-                300: '#7AC1E4',
-                400: '#47A9DA',
-                500: '#0088CC',
-                600: '#007AB8',
-                700: '#006BA1',
-                800: '#005885',
-                900: '#003F5E',
                 1: '#5D576B',
                 2: '#9BC1BC',
                 3: '#ED6A5A',
+                3: '#E6EBE0',
+                3: '#F4F1BB',
             },
             // Redefining only one shade, rest of the color will remain same.
             amber: {

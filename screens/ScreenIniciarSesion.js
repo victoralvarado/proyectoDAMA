bea1df46826e90fc6d202859bf8e09ff8c224100
Victor Alvarado <alvaradoguzman900@gmail.com>
2022-10-16 10:34:30 -0600

    Update ScreenIniciarSesion.js

--- screens/ScreenIniciarSesion.js
+++ screens/ScreenIniciarSesion.js
@@ -4,20 +4,18 @@ import { ListItem, Avatar } from "react-native-elements";
 import { Input, Stack, Center, NativeBaseProvider, FormControl, extendTheme, HStack, Link, Icon, } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import * as yup from 'yup'
 export default function ScreenInicio(props) {
-    function press() {
-        console.log("inicio");
-    }
+
     const [show, setShow] = React.useState(false);
-    const SignupSchema = Yup.object().shape({
-        correo: Yup
+    const SignupSchema = yup.object().shape({
+        correo: yup
             .string()
             .email('Ingrese un correo valido')
             .required('El correo es requerido'),
-        clave: Yup
+        clave: yup
             .string()
-            .required('La calve es requerida')
+            .required('La clave es requerida')
             .matches(
                 /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
                 "Debe contener 8 caracteres, uno en mayúscula, uno en minúscula, un número y un carácter especial"
@@ -25,7 +23,6 @@ export default function ScreenInicio(props) {
     });
     const theme = extendTheme({
         colors: {
-            // Add new color
             primary: {
                 1: '#5D576B',
                 2: '#9BC1BC',
@@ -47,83 +44,146 @@ export default function ScreenInicio(props) {
             }}
             validationSchema={SignupSchema}
             onSubmit={values => {
-                // same shape as initial values
                 console.log(values);
             }}
         >
-            <SafeAreaView style={styles.container}>
-                <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} style={styles.scrollView}>
-                    <NativeBaseProvider theme={theme}>
-                        <Center flex={1} px="3">
-                            <View>
-                                <Text style={styles.text}>Iniciar Sesión</Text>
-                            </View>
-                            <View style={{ marginTop: 14 }}>
-                                <Text style={{ fontFamily: "Poppins Medium", fontSize: 16, color: "#5D576B", marginStart: 55, marginEnd: 55, textAlign: 'center' }}>
-                                    Bienvenido de Regreso, inicia Sesión para continuar
-                                </Text>
-                            </View>
-                            <FormControl w="100%" maxW="400px" mx="auto" style={{ marginTop: 44 }}>
-                                <FormControl.Label _text={{
-                                    fontSize: 15,
-                                    fontWeight: "500",
-                                    fontFamily: "Poppins Medium",
-                                    color: "primary.1"
-                                }} >Correo</FormControl.Label>
-                                <Input style={{
-                                    color: "#5D576B"
-                                }} _focus={{
-                                    backgroundColor: "primary.2",
-                                }} size="2xl" variant="outline" placeholder="Correo" />
-                            </FormControl>
-                            <FormControl w="100%" maxW="400px" mx="auto" style={{ marginTop: 10 }}>
-                                <FormControl.Label _text={{
-                                    fontSize: 15,
-                                    fontFamily: "Poppins Medium",
-                                    color: "primary.1"
-                                }} >Clave</FormControl.Label>
-                                <Input style={{
-                                    color: "#5D576B"
-                                }} _focus={{
-                                    backgroundColor: "primary.2",
-                                }} size="2xl" variant="outline" placeholder="Clave" type={show ? "text" : "password"} InputRightElement={<Pressable onPress={() => setShow(!show)}>
-                                    <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
-                                </Pressable>} />
-                            </FormControl>
-                            <View style={{ marginTop: 50, alignItems: "center" }}>
-                                <Pressable style={({ pressed }) => [
+            {({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
+                <SafeAreaView style={styles.container}>
+                    <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} style={styles.scrollView}>
+                        <NativeBaseProvider theme={theme}>
+                            <Center flex={1}>
+
+                                <View>
+                                    <Text style={styles.text}>Iniciar Sesión</Text>
+                                </View>
+
+                                <View style={{ marginTop: 14 }}>
+                                    <Text style={{ fontFamily: "Poppins Medium", fontSize: 16, color: "#5D576B", marginStart: 55, marginEnd: 55, textAlign: 'center' }}>
+                                        Bienvenido de Regreso, inicia Sesión para continuar
+                                    </Text>
+                                </View>
+
+                                {/* Inicio Control Correo */}
+                                <FormControl w="100%" maxW="390px" style={{ marginTop: 44 }}>
+
+                                    {/* Inicio Label Correo */}
+                                    <FormControl.Label _text={{ fontSize: 15, fontWeight: "500", fontFamily: "Poppins Medium", color: "primary.1" }} >
+                                        Correo
+                                    </FormControl.Label>
+                                    {/* Fin Label Correo */}
+
+                                    {/* Inicio Input Correo */}
+                                    <Input
+                                        style={{ color: "#5D576B" }}
+                                        value={values.correo}
+                                        onChangeText={handleChange('correo')}
+                                        onBlur={() => setFieldTouched('correo')}
+                                        _focus={{
+                                            backgroundColor: "primary.2",
+                                        }}
+                                        size="2xl"
+                                        variant="outline"
+                                        placeholder="Correo"
+                                    />
+                                    {/* Fin Input Correo */}
+
+                                    {/* Inicio Advertencias de validacion */}
                                     {
-                                        backgroundColor: pressed
-                                            ? 'rgb(210, 230, 255)'
-                                            : '#9BC1BC',
-                                        fontFamily: "",
-                                        width: 265,
-                                        height: 64,
-                                        borderRadius: 15,
-                                        alignItems: "center",
-                                        justifyContent: 'center'
-                                    },
-                                    styles.wrapperCustom
-                                ]} onPress={press}>
-                                    <Text style={{ fontFamily: "Poppins SemiBold", fontSize: 16, color: "#5D576B" }}>Iniciar Sesión</Text>
-                                </Pressable>
-                            </View>
-                            <HStack mt="6" style={{ marginTop: 14 }} justifyContent="center">
-                                <Text style={{ fontFamily: "Poppins Regular", fontSize: 14, color: "#5D576B" }}>
-                                    ¿No tienes una cuenta?{" "}
-                                </Text>
-                                <Link _text={{
-                                    color: "primary.3",
-                                    fontFamily: "Poppins Regular",
-                                    fontSize: 14
-                                }} onPress={() => props.navigation.navigate("ScreenCrearCuenta")}>
-                                    Crear cuenta
-                                </Link>
-                            </HStack>
-                        </Center>
-                    </NativeBaseProvider>
-                </ScrollView>
-            </SafeAreaView>
+                                        touched.correo && errors.correo &&
+                                        <Text style={{ fontSize: 12, color: '#ED6A5A', fontFamily: 'Poppins Medium' }}>{errors.correo}</Text>
+                                    }
+                                    {/* Fin Advertencias de validacion */}
+
+                                </FormControl>
+                                {/* Fin Control Correo */}
+
+                                {/* Inicio Control Clave */}
+                                <FormControl w="100%" maxW="390px" style={{ marginTop: 10 }}>
+
+                                    {/* Inicio Label Clave */}
+                                    <FormControl.Label _text={{ fontSize: 15, fontFamily: "Poppins Medium", color: "primary.1" }} >
+                                        Clave
+                                    </FormControl.Label>
+                                    {/* Fin Label Clave */}
+
+                                    {/* Inicio Input Clave */}
+                                    <Input
+                                        style={{
+                                            color: "#5D576B"
+                                        }}
+                                        value={values.clave}
+                                        onChangeText={handleChange('clave')}
+                                        onBlur={() => setFieldTouched('clave')}
+                                        _focus={{
+                                            backgroundColor: "primary.2",
+                                        }}
+                                        size="2xl"
+                                        variant="outline"
+                                        placeholder="Clave"
+                                        type={show ? "text" : "password"}
+                                        InputRightElement={
+                                            <Pressable onPress={() => setShow(!show)}>
+                                                <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="primary.1" />
+                                            </Pressable>
+                                        }
+                                    />
+                                    {/* Fin Input Clave */}
+
+                                    {/* Inicio Advertencias de validacion */}
+                                    {
+                                        touched.clave && errors.clave &&
+                                        <Text style={{ fontSize: 12, color: '#ED6A5A', fontFamily: 'Poppins Medium' }}>{errors.clave}</Text>
+                                    }
+                                    {/* Fin Advertencias de validacion */}
+
+                                </FormControl>
+                                {/* Fin Control Clave */}
+
+                                {/* Inicio Boton Iniciar Sesión */}
+                                <View style={{ marginTop: 50, alignItems: "center" }}>
+                                    <Pressable
+                                        style={({ pressed }) => [
+                                            {
+                                                backgroundColor: pressed
+                                                    ? 'rgb(210, 230, 255)'
+                                                    : '#9BC1BC',
+                                                fontFamily: "",
+                                                width: 265,
+                                                height: 64,
+                                                borderRadius: 15,
+                                                alignItems: "center",
+                                                justifyContent: 'center'
+                                            },
+                                            styles.wrapperCustom
+                                        ]}
+                                        onPress={handleSubmit} disabled={!isValid}>
+                                        <Text style={{ fontFamily: "Poppins SemiBold", fontSize: 16, color: "#5D576B" }}>Iniciar Sesión</Text>
+                                    </Pressable>
+                                </View>
+                                {/* Fin Boton Iniciar Sesión */}
+
+                                {/* Texto y Link para ir a crear una cuanta*/}
+                                <HStack mt="6" style={{ marginTop: 14 }} justifyContent="center">
+                                    <Text style={{ fontFamily: "Poppins Regular", fontSize: 14, color: "#5D576B" }}>
+                                        ¿No tienes una cuenta?{" "}
+                                    </Text>
+                                    <Link
+                                        _text={{
+                                            color: "primary.3",
+                                            fontFamily: "Poppins Regular",
+                                            fontSize: 14
+                                        }}
+                                        onPress={() => props.navigation.navigate("ScreenCrearCuenta")}>
+                                        Crear cuenta
+                                    </Link>
+                                </HStack>
+                                {/* Fin Texto y Link para ir a crear una cuanta*/}
+
+                            </Center>
+                        </NativeBaseProvider>
+                    </ScrollView>
+                </SafeAreaView>
+            )}
         </Formik>
     );
 };
